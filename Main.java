d0e5d9b82e699659534db6d97da75a482cb626e8
csnpch <ChitsanuphongChaihong@outlook.com>
2022-05-10 00:46:05 +0700

    1.0.0

---
+++ Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //? Create a new file
+        ManageFile productFile = new ManageFile("product");
+        // ManageFile sellerFile = new ManageFile("seller");
+    
+        //? product
+        String[] dataArr = {"1", "Hu", "hu", "hu"};
+        productFile.writeDataByArr(dataArr);
+
+        productFile.writeDataByStr("2, Ha, ha, ha");
+        productFile.writeDataByStr("3, He, he, he");
+        productFile.writeDataByStr("4, Ho, ho, ho");
+        // productFile.readFile();
+        // productFile.reportData();
+        // productFile.clearFile();
+
+        
+        //? Example Get Data Product
+        String[][] dataProduct = productFile.getData();
+        
+        for (int r = 0; r < dataProduct.length; r++) {
+            for (int c = 0; c < dataProduct[r].length; c++) {
+                System.out.print(dataProduct[r][c] + " ");
+            }
+            System.out.println();
+        }
+
+        //? edit data to product
+        dataProduct[0][1] = "HuHaHe";
+        productFile.writeDataNewToFile(dataProduct);
+
+
+        //? customer
+        // customerFile.writeDataByStr("So, la, su");
+        // customerFile.writeDataByStr("Su, la, so");
+        // customerFile.readFile();
+    
+            
+    }
+
+}
---
+++ ManageFile.java
@@ -0,0 +1,240 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Base64;
+
+//- Easy to use "Manage File" by Sama Ham (fb.com/csnp.ch)
+
+public class ManageFile {
+
+    private String[][] data;
+    private int indexData = 0;
+    private String fileName = new String(""); 
+    private String typeName = new String("md");
+
+
+    public ManageFile() {
+        if (this.fileName.isEmpty()) {
+            System.out.println("\n\nParameter file name is empty!\n\n");
+        }
+    }
+
+
+    public ManageFile(String name) {
+        this.fileName = name;
+        checkDirectory();
+        createFile(); 
+    }
+
+
+    public ManageFile(String name, String type) { 
+        this.fileName = name;
+        this.typeName = type;
+        checkDirectory();
+        createFile(); 
+    }
+
+
+    public void checkDirectory() {
+        File folder = new File("data");
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+    }
+
+
+    public String[][] getData() {
+        readFile();
+        return this.data;
+    }
+
+
+    public void setSizeData(int row, int col) {
+        this.data = new String[row][col];
+        System.out.println("Array size data in file : " + row + " x " + col);
+    }
+
+
+    public void clearData() {
+        for (int i = 0; i < this.data.length; i++) {
+            for (int j = 0; j < this.data[i].length; j++) {
+                this.data[i][j] = "";
+            }
+        }
+    }
+
+
+    public void clearFile() {
+
+        File file = new File("./data/" + this.fileName + "." + this.typeName);
+        try {
+            file.delete();
+        } catch (Exception e) {
+            System.out.println("File not found");
+        }
+        
+    }
+
+    
+    public void pushDataToVariable(String[] val) {
+
+        for (int i = 0; i < val.length; i++) {
+            this.data[this.indexData][i] = val[i];
+        }
+        this.indexData++;
+
+    }
+
+
+    public void reportData() {
+        for (int i = 0; i < this.data.length; i++) {
+            for (int j = 0; j < this.data[i].length; j++) {
+                System.out.print(this.data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void readFile() {
+
+        try {
+
+            if (this.data != null) {
+                clearData();
+            }
+        
+            File myObj = new File("./data/"+ this.fileName + "." + this.typeName);
+            Scanner myReaderForCount = new Scanner(myObj);
+            Scanner myReaderForData = new Scanner(myObj);
+
+            int unitRows = 0;
+            int unitCols = 0;
+            while (myReaderForCount.hasNextLine()) {
+                String data = myReaderForCount.nextLine();
+                data = getDecoded(data);
+                unitRows++;
+                unitCols = data.split(",").length;
+                data = data.replaceAll("\\s+", "");
+            }
+            setSizeData(unitRows, unitCols);
+            
+            //- It bin 
+            while (myReaderForData.hasNextLine()) {
+                String data = myReaderForData.nextLine();
+                data = getDecoded(data);
+                data = data.replaceAll("\\s+", "");
+                if (data.split(",").length > 0) {
+                    pushDataToVariable(data.split(","));
+                }
+            }
+
+            myReaderForCount.close();
+            myReaderForData.close();
+        
+        } catch (FileNotFoundException e) {
+            
+            createFile();
+            readFile();
+
+        }
+    }
+
+
+    public void createFile() {
+        try {
+
+            File myObj = new File("./data/" + this.fileName + "." + this.typeName);
+            
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println(this.fileName + " - File already exists.");
+            }
+
+        } catch (IOException ex) {
+            
+            System.out.println("An error occurred.");
+            // ex.printStackTrace();
+        
+        }
+    }
+
+
+    public void writeDataByArr(String[] data) {
+        
+        String line = "";
+        for (int i = 0; i < data.length; i++) {
+            line += data[i] + ",";
+        }
+        line = line.substring(0, line.length() - 1);
+        writeDataByStr(line);
+
+    }
+
+
+    public void writeDataByStr(String val) {
+        try {
+
+            val = getEncoded(val);
+            
+            File myObj = new File("./data/" + this.fileName + "." + this.typeName);
+            FileWriter myWriter = new FileWriter(myObj, true);
+            BufferedWriter buffer = new BufferedWriter(myWriter);
+            buffer.write(val);
+            buffer.newLine();
+            buffer.close();
+            myWriter.close();
+        }
+        catch (IOException ex) {
+            System.out.println("An error occurred.");
+        }
+    }
+
+
+    public String getEncoded(String val) {
+        // return val;
+        try {
+            byte[] data = val.getBytes("UTF-8");
+            String encoded = Base64.getEncoder().encodeToString(data);
+            return encoded;
+        } catch (IOException ex) {
+            System.out.println("An error occurred.");
+            return "";
+        }
+    }
+
+
+    public String getDecoded(String val) {
+        // return val;
+        try {
+            byte[] data = val.getBytes("UTF-8");
+            String decoded = new String(Base64.getDecoder().decode(data), "UTF-8");
+            return decoded;
+        } catch (IOException ex) {
+            System.out.println("An error occurred.");
+            return val;
+        }
+    }
+
+    
+    public void writeDataNewToFile(String[][] data) {
+        
+        clearFile();
+
+        for (int r = 0; r < data.length; r++) {
+
+            String line = "";
+            for (int c = 0; c < data[r].length; c++) {
+                line += data[r][c] + ",";
+            }
+            line = line.substring(0, line.length() - 1);
+            writeDataByStr(line);
+        }
+
+    } 
+
+}
